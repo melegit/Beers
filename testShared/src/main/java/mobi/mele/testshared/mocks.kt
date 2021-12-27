2540a190441b6d4ebb91d038b572ae03f4f9b569
mele <mele@yahoo.es>
2021-12-27 16:32:09 +0100

    Unit Test
    - añadido test para MainViewModel y para DetailViewModel.
    - añadidas dependecias necesarias
    - añadido módulo de mocks de objetos

---
+++ testShared/src/main/java/mobi/mele/testshared/mocks.kt
@@ -0,0 +1,9 @@
+package mobi.mele.testshared
+
+import mobi.mele.domain.dto.Beer
+
+val mockedBeer = Beer(id = 5,
+    name = "Avery Brown Dredge",
+    abv = 3.5,
+    description = "An Imperial Pilsner in c…e people who make them." ,
+    image_url = "https://images.punkapi.com/v2/5.png")
