48780e6d08624397a22c1326fd38f6867120c7d5
mele <mele@yahoo.es>
2023-08-16 23:54:59 +0200

    [WIP] change the itemView of a recyclerView to compose

--- app/src/main/java/mobi/mele/beers/ui/main/adapter/BeersAdapter.kt
+++ app/src/main/java/mobi/mele/beers/ui/main/adapter/BeersAdapter.kt
@@ -1,13 +1,29 @@
 package mobi.mele.beers.ui.main.adapter
 
-import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import mobi.mele.beers.R
-import mobi.mele.beers.databinding.BeerItemBinding
+import coil.compose.rememberAsyncImagePainter
 import mobi.mele.beers.extensions.basicDiffUtil
-import mobi.mele.beers.extensions.inflate
 import mobi.mele.domain.dto.Beer
 
 /**
@@ -15,16 +31,15 @@ import mobi.mele.domain.dto.Beer
  * date   : 25/12/21
  * e-mail : meleappdev@gmail.com
  */
-class BeersAdapter(private val beerClickedListener: (Beer) -> Unit)  :
+class BeersAdapter(private val beerClickedListener: (Beer) -> Unit) :
     RecyclerView.Adapter<BeersAdapter.ViewHolder>() {
 
-    var beers: List<Beer> by basicDiffUtil(emptyList(), areContentsTheSame = {
-        old, new -> old.id == new.id
+    var beers: List<Beer> by basicDiffUtil(emptyList(), areContentsTheSame = { old, new ->
+        old.id == new.id
     })
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = parent.inflate(R.layout.beer_item, false)
-        return ViewHolder(view)
+        return ViewHolder(ComposeView(parent.context), beerClickedListener)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -33,16 +48,67 @@ class BeersAdapter(private val beerClickedListener: (Beer) -> Unit)  :
         holder.itemView.setOnClickListener { beerClickedListener(beer) }
     }
 
-    override fun getItemCount(): Int =beers.size
+    override fun getItemCount(): Int = beers.size
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        private val binding = BeerItemBinding.bind(view)
+    override fun onViewRecycled(holder: ViewHolder) {
+        holder.composeView.disposeComposition()
+    }
+
+    class ViewHolder(
+        val composeView: ComposeView,
+        private val beerClickedListener: (Beer) -> Unit
+    ) : RecyclerView.ViewHolder(composeView) {
+        init {
+            composeView.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        }
+
         fun bind(beer: Beer) {
-            binding.tvBeerNameBeerName.text = beer.name
+            composeView.setContent {
+                MaterialTheme {
+                    BeerItem(
+                        modifier = Modifier.clickable { beerClickedListener(beer) },
+                        beer = beer
+                    )
+                }
+            }
+            /*binding.tvBeerNameBeerName.text = beer.name
             Glide
                 .with(binding.root.context)
                 .load(beer.image_url)
-                .into(binding.imageBeer)
+                .into(binding.imageBeer)*/
+        }
+    }
+}
+
+@Composable
+fun BeerItem(modifier: Modifier = Modifier, beer: Beer) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color.White)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .height(200.dp)
+                    .padding(top = 20.dp),
+                painter = rememberAsyncImagePainter(model = beer.image_url),
+                contentDescription = null
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp),
+                text = beer.name,
+                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
