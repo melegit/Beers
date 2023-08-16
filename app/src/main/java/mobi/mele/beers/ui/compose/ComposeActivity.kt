613460efbdcdfa087fa1077f2aadb423a9c6f3fa
mele <mele@yahoo.es>
2023-08-16 22:39:19 +0200

    [WIP] Use of ComposeActivity

--- app/src/main/java/mobi/mele/beers/ui/compose/ComposeActivity.kt
+++ app/src/main/java/mobi/mele/beers/ui/compose/ComposeActivity.kt
@@ -3,13 +3,21 @@ package mobi.mele.beers.ui.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import mobi.mele.beers.R
 import mobi.mele.beers.ui.compose.ui.theme.BeersTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Greeting(stringResource(R.string.activity_compose_title))
+                        Greeting(
+                            stringResource(R.string.activity_compose_description_info),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                                .align(Alignment.CenterHorizontally)
+                        )
+                    }
                 }
             }
         }
@@ -32,7 +52,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "$name!",
         modifier = modifier
     )
 }
