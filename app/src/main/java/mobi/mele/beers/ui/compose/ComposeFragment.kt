34afe48b9b1809136a53387ae7a7d54e0e014721
mele <mele@yahoo.es>
2023-08-16 23:17:01 +0200

    [WIP] extensión fragment to simplify
    - extension function to simplify the use of composable fragment.

--- app/src/main/java/mobi/mele/beers/ui/compose/ComposeFragment.kt
+++ app/src/main/java/mobi/mele/beers/ui/compose/ComposeFragment.kt
@@ -10,11 +10,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.fragment.app.Fragment
+import mobi.mele.beers.R
 
 class ComposeFragment : Fragment() {
     override fun onCreateView(
@@ -22,25 +25,32 @@ class ComposeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                // When we use Jetpack Compose in components that aren't an activity, we need to associate
-                // the lifecycle of the component with the composable we are creating. We do this with setViewCompositionStrategy.
-                // for fragments, the ideal argument is ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed.
-                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = "I'm a compose fragment",
-                        style = MaterialTheme.typography.titleLarge,
-                        textAlign = TextAlign.Center,
-                    )
-                }
+        return composeView {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(R.string.fragment_compose),
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center,
+                )
             }
         }
     }
+}
+
+fun Fragment.composeView(content: @Composable () -> Unit): ComposeView {
+    return ComposeView(requireContext()).apply {
+        setContent {
+            // When we use Jetpack Compose in components that aren't an activity, we need to associate
+            // the lifecycle of the component with the composable we are creating. We do this with setViewCompositionStrategy.
+            // for fragments, the ideal argument is ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed.
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            content()
+        }
+    }
+
 }
